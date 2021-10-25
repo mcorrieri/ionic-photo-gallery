8c962eb32e7c9d0670b5280e566f2def5e16f502
Michael <Mlcorrieri@gmail.com>
2021-10-24 21:00:27 -0400

    photo viewer added

--- src/hooks/usePhotoGallery.tsx
+++ src/hooks/usePhotoGallery.tsx
@@ -10,17 +10,34 @@ import {
 import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Storage } from "@capacitor/storage";
 import { Capacitor } from "@capacitor/core";
+import { Interface } from "readline";
 
 export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
   const takePhoto = async () => {
     const cameraPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
+    const fileName = new Date().getTime() + ".jpeg";
+    const newPhotos = [
+      {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath,
+      },
+      ...photos,
+    ];
+    setPhotos(newPhotos);
   };
 
   return {
+    photos,
     takePhoto,
   };
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
 }
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -19,7 +19,7 @@ import "./Tab2.css";
 import { usePhotoGallery } from "../hooks/usePhotoGallery";
 
 const Tab2: React.FC = () => {
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -28,6 +28,17 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonContent>
+          <IonGrid>
+            <IonRow>
+              {photos.map((photo, index) => (
+                <IonCol size="6" key={index}>
+                  <IonImg src={photo.webviewPath} />
+                </IonCol>
+              ))}
+            </IonRow>
+          </IonGrid>
+        </IonContent>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
